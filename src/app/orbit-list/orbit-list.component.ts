fa49ebeab3fe3ee069eb27943d70268aec7b4fbc
Michael <michaelyoung451@gmail.com>
2019-12-18 19:01:29 -0600

    added sort event

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -7,8 +7,21 @@ import { Satellite } from '../satellite';
   styleUrls: ['./orbit-list.component.css']
 })
 export class OrbitListComponent implements OnInit {
+
   @Input() satellites: Satellite[]
   constructor() { }
+
+  sort(column: string): void {
+    // array.sort modifies the array, sorting the items based on the given compare function
+    this.satellites.sort(function(a: Satellite, b: Satellite): number {
+       if(a[column] < b[column]) {
+          return -1;
+       } else if (a[column] > b[column]) {
+          return 1;
+       }
+       return 0;
+    });
+ }
 
   ngOnInit() {
   }
